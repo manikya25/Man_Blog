615a66c4eefa2d441b607fd9763684cab9e9c5ea
manikya25 <anvekarmanikya@gmail.com>
2024-11-28 21:04:44 +0530

    second commit for blog project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import 'normalize.css';
 import { Buffer } from 'buffer';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
